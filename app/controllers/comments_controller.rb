22f04f178989c7b2584575630b394d8ea574d804
hasan <hasanbatis440@gmail.com>
2021-08-28 21:15:04 +0300

    adding comment feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user
+  before_action :set_comment, only:[:create, :destroy]
+  before_action :set_meeting
+
+  def new
+   @comment = Comment.new
+  end
+
+  def create
+    @comment = @meeting.comments.create(params[:comment].permit(:reply, :meeting_id))
+    @comment.user_id = current_user.id
+
+    respond_to do |format|
+      if @comment.save
+      format.html {redirect_to meeting_path(@meeting)}
+      format.js
+    else
+      format.html {redirect_to meeting_path(@meeting), notice: "Your commentdid not saved"}
+      ormat.js
+    end
+  end
+  end
+
+  def destroy
+    @comment = @meeting.comments.find(params[:id])
+    @comment.destroy
+    redirect_to meeting_path(@meeting)
+  end
+
+  private
+  def set_meeting
+    @meeting = Meeting.find(params[:meeting_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:reply  )
+  end
+
+end
--- db/migrate/20210828143801_create_comments.rb
+++ db/migrate/20210828143801_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :reply
-      t.belongs_to :meetings, null: false, foreign_key: true
+      t.belongs_to :meeting, null: false, foreign_key: true
 
       t.timestamps
     end
